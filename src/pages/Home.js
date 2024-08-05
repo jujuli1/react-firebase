f4e1ea73ad3c9eec63bf407df67277c81fe43a5a
LEFEVRE <lefevrejulien@live.fr>
2024-08-05 09:44:51 +0200

    final

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,8 +7,10 @@ const Home = () => {
     const {currentUser} =  useContext(UserContext)
     return (
         <div className='container p-5'>
-            <h1 className='display-2 text-light'>Inscrivez vous ou connectez vous</h1>
+            <h1 className='display-2 text-light'>
             {currentUser ? "Welcome" : "Inscrivez vous ou connectez vous" }
+            </h1>
+            
         </div>
     );
 };
