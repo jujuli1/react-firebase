4241220e8ed4cfc96fe5778dfe3c089a176d2e8c
LEFEVRE <lefevrejulien@live.fr>
2024-08-06 23:44:49 +0200

    style

--- src/pages/Private/PrivateHome/PrivateHome.js
+++ src/pages/Private/PrivateHome/PrivateHome.js
@@ -91,7 +91,7 @@ const PrivateHome = () => {
             marginTop: '3px',
           }}
         /></Link>
-            <img src={gif} alt='cafe bebe yoda' class="rounded-pill ms-5 " style={{ position: 'relative', border: '2px solid grey' }}/>
+            <img src={gif} alt='cafe bebe yoda' class="rounded-pill ms-5 " style={{ position: 'relative', border: '2px solid grey', boxShadow: '0px 4px 8px rgba(0, 0, 255, 0.5)' }}/>
             <button className='github' onClick={moveElement}>Mon github</button>
             </div>
             
