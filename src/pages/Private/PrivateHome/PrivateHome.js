2090a71eca935c4446e197c6bc2b47272286b517
LEFEVRE <lefevrejulien@live.fr>
2024-08-08 12:18:35 +0200

    style

--- src/pages/Private/PrivateHome/PrivateHome.js
+++ src/pages/Private/PrivateHome/PrivateHome.js
@@ -25,23 +25,25 @@ const PrivateHome = () => {
     const [animated, setAnimated] = useState(false);
     const transformStyle = `translate(100px, 100px) rotate(${position}deg) translate(-100px, -100px)`;
     return (
-        <div className='container p-5'>
+        <div className='container p-5' style={{ display: 'flex', flexDirection: 'row-reverse' }}>
 
-            <div style={{ marginLeft: '600px', marginTop: '50px' }}>
-                <div
+
+            {/** div de decoration crocher */}
+            <div style={{ marginLeft: '585px', marginTop: '50px' }}>
+               {/*<div
                 style={{
                     backgroundColor: 'dark',
                     width: '113px',
                     height: '55px',
                     position: 'absolute',
-                    marginTop: '132px',
-                    marginLeft: '100px',
+                    marginTop: '134px',
+                    marginLeft: '85px',
                     borderTop: '8px solid grey',
                     borderBottom: '8px solid grey',
                     borderRadius: '10px',
                     
                 }}
-                    ></div>
+                    ></div> */} 
                     <div
                 style={{
                     backgroundColor: 'dark',
@@ -49,14 +51,17 @@ const PrivateHome = () => {
                     height: '55px',
                     position: 'absolute',
                     marginTop: '129px',
-                    marginLeft: '327px',
+                    marginLeft: '268px',
                     borderTop: '8px solid grey',
                     borderBottom: '8px solid grey',
-                    borderRadius: '10px',
+                    borderRadius: '5px',
                     
                 }}
                     ></div>
-            <Link
+
+                    {/**lien Me joindre */}
+           {/**
+            *<Link
       to="/coordonnee"
       className='btn btn-primary rounded-left-custom'
       style={{
@@ -78,8 +83,12 @@ const PrivateHome = () => {
       }}
     >
       Me joindre
-    </Link>
-            <Link to="/competences" className='btn btn-primary rounded-right-custom btn-transition' style={{boxShadow: '0px 4px 8px rgba(0, 0, 0, 1)',}}>Compétences</Link>
+    </Link> 
+            * 
+            */} 
+
+    {/**lien competences */}
+            <Link to="/coordonnee" className='btn btn-primary rounded-right-custom btn-transition' style={{ boxShadow: '0px 0px 8px rgba(255, 0, 0, 1)' }}>Me joindre</Link>
             <Link to="https://github.com/jujuli1" className='btn btn-primary moving-element ' style={{transform: transformStyle,rotate: '90deg', position: 'absolute', marginTop: '39px',marginLeft: '105px', width: '73px', borderRadius: "30px"}}><img
           src="https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png" // url logo GitHub
           alt="GitHub"
@@ -90,17 +99,23 @@ const PrivateHome = () => {
             borderRadius: '25px',
             marginTop: '3px',
           }}
-        /></Link>
+        />
+        
+        
+        {/**lien github */}
+        </Link>
             <img src={gif} alt='cafe bebe yoda' class="rounded-pill ms-5 " style={{ position: 'relative', border: '2px solid grey', boxShadow: '0px 4px 8px rgba(0, 0, 255, 0.5)' }}/>
             <button className='github' onClick={moveElement}>Mon github</button>
             </div>
-            
-            <h1 className='display-3 text-light mb-4'>
-                Madame, Monsieur ...
-            </h1>
-            <div class="row justify-content-center">
-        <div class="col-md-8">
-            <p class="text-light bg-dark p-3 rounded fs-4 fondu custom-line-height fs-3 text-center">
+
+            {/**texte de presentation */}
+            <div className='presentation' style={{ marginLeft: "-190px", marginRight: "-370px", height: "817px", overflowY: "scroll", overflowX: "hidden"}}>
+    <h1 className='text-light mb-4' style={{ width: "225px" }}>
+        Madame, Monsieur ...
+    </h1>
+    <div className="row justify-content-center">
+        <div className="col-md-8">
+            <p className="text-light bg-dark p-3 rounded fs-4 fondu custom-line-height fs-3 text-center" style={{ width: "152%", marginLeft: "-143px" }}>
                 Actuellement en reconversion professionnelle dans les métiers du numérique, et ayant obtenu le titre professionnel de développeur web et web mobile avec l'école O'clock, je suis actuellement à la recherche d'une première expérience professionnelle, d'une formation en alternance ou d'un stage dans ce domaine.
 
                 Je suis motivé par la volonté d'exercer un métier dans un domaine que j'affectionne et que je pratique depuis longtemps en tant qu'amateur : l'informatique et les métiers du numérique en général.
@@ -109,13 +124,12 @@ const PrivateHome = () => {
 
                 Je vous remercie de l'attention portée à ma réalisation, et, dans l'attente d'une réponse de votre part, je vous adresse mes sincères salutations.
 
-                <br/><br/>
+                <br /><br />
                 LEFEVRE Julien
             </p>
-
-            
         </div>
     </div>
+</div>
             
              
         </div>
