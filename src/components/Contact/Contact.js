3e310843eefcda192d51ab2044e6cc9f8e18c536
LEFEVRE <lefevrejulien@live.fr>
2024-08-11 22:46:30 +0200

    style

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -35,19 +35,19 @@ const Contact = () => {
         <section className='contact'>
             <form onSubmit={onSubmit}>
 
-                <h2>Me contacter</h2>
+                
             <div className='input-box'>
-                <label>Votre nom</label>
+                <label className="futuristic">Votre nom</label>
                 <input type='text' className='field' placeholder='Entrez votre nom' name='name' required/>
             </div>
 
             <div className='input-box'>
-                <label>Votre adresse mail</label>
+                <label className="futuristic">Votre adresse mail</label>
                 <input type='email' className='field' placeholder='Entrez votre adresse mail' name='email' required/>
             </div>
 
             <div className='input-box'>
-                <label>Votre message</label>
+                <label className="futuristic">Votre message</label>
                 <textarea name='message'  className='field mess' placeholder='Votre message' required></textarea>
             </div>
             <button type='submit'>Envoyer</button>
